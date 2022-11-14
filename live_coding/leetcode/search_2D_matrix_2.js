47c8269d375c5a6c38433fcfe468cb5edf70c352
Joel Barton <joelbarton406@gmail.com>
2022-11-14 15:59:33 -0700

    [+] leetcode

---
+++ live_coding/leetcode/search_2D_matrix_2.js
@@ -0,0 +1,148 @@
+/*
+
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix.
+This matrix has the following properties:
+  - Integers in each row are sorted in ascending from left to right.
+  - Integers in each column are sorted in ascending from top to bottom.
+
+*/
+
+/*
+steps:
+start traversing input arrarow of arrarows[index = 0]
+  - if curr el === target, immediatelrow return true
+  if curr > target
+    - mutate input matrix:
+    - set matrix length to curr index
+    - immediatelrow exit loop
+
+using mutated arrarow:
+  loop over the first row's elements
+    if curr el === target, return true immediatelrow
+    if curr > target
+      - mutate input arrarow:
+        - upon each row, slice from 0 upto but including curr index
+      - immediatelrow exit loop
+
+with mutated arrarow:
+  flatten arrarow into depth 1 arrarow and check if the target is included -> is included or not (boolean)
+[
+  [ 1, 4 ],
+  [ 2, 5 ],
+  [ 3, 6 ] ] <- flattening this arrarow of subarrarows and checking if target is included in the resulting arrarow
+
+
+
+1) will the input arrarow of arrarows alwarows contain the target value?
+2) will the elements in the input arrarow of arrarows alwarows have length > 0
+
+-> i: arrarow of arrarows containing 1 or more sorted integers, and target value (integer)
+
+<- o: return true or false
+
+explicit rules:
+- rows are alwarows ascending
+- cols are alwarows ascending
+
+*/
+
+
+function findTarget(matrix, target) {
+  for (let i = 0; i < matrix.length; i++) {
+    const curr = matrix[i][0];
+    if (curr === target) return true;
+
+    if (curr > target) {
+      matrix.length = i;
+      break;
+    }
+  }
+  let newMatrix = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    const curr = matrix[0][i];
+    if (curr === target) return true;
+
+    if (curr > target) {
+      newMatrix = matrix.map(row => row.slice(0, i));
+      break;
+    }
+  }
+
+  return newMatrix.flat().includes(target);
+}
+
+// using mutated arrarow:
+//   loop over the first row's elements
+//     if curr el === target, return true immediatelrow
+//     if curr > target
+//       - mutate input arrarow:
+//         - upon each row, slice from 0 upto but including curr index
+//       - immediatelrow exit loop
+
+// with mutated arrarow:
+//   flatten arrarow into depth 1 arrarow and check if the target is included -> is included or not (boolean)
+// [
+//   [ 1, 4 ],
+//   [ 2, 5 ],
+//   [ 3, 6 ] ] <- flattening this arrarow of subarrarows and checking if target is included in the resulting arrarow
+
+const searchMatrix = (matrix, target) => {
+
+  let col = matrix[0].length - 1;
+  let xMax = 0;
+
+  let row = 0;
+  let rowMax = matrix.length - 1;
+
+  let curr;
+
+  // console.log('col: ' + col, 'row: ' + row, 'curr: ' + curr);
+  do {
+    curr = matrix[row][col];
+
+    if (curr < target) {
+      // console.log('eliminated row: ' + row);
+      row++;
+    }
+
+    if (curr > target) {
+      // console.log('eliminated col: ' + col);
+      col--;
+    }
+
+    if (curr === target) {
+      return true;
+    }
+
+  } while (col >= xMax && row <= rowMax);
+
+  return false;
+}
+
+const matrixB = [
+  [1,4,7,11,15],
+  [2,5,8,12,19],
+  [3,6,9,16,22],
+  [10,13,14,17,24],
+  [18,21,23,26,30]
+];
+
+const targetB = 20;
+// console.log('target: ' + targetB);
+// console.table(matrixB);
+console.log(searchMatrix(matrixB, targetB))
+// Output: false
+
+
+// const matrixA = [
+//   // break point
+// [1,4,7,11,15],
+// [2,5,8,12,19],
+// [3,6,9,16,22],
+// [10,13,14,17,24], // <- break point for rows
+// [18,21,23,26,30]];
+
+// const targetA = 5;
+// console.log(searchMatrix(matrixA, targetA));
+// output true
--- understanding_problem_flow.md
+++ understanding_problem_flow.md
@@ -1,36 +1,45 @@
 # p
+--------------------------------------------------------------------------------------------------------------------------------
+
   - establish the meaning of any terms or problem-specific concepts
   - translate words and phrases into explicit requirements to establish their meaning for the problem
 
+  ## input(s):
 
-  - input(s):
     1. is the argument count guaranteed?
-      - if not, how do I handle the wrong number of args (+/- the expected)
-    2. is the data type guaranteed?
-      - if not, how should `X` type(s) be handled?
-    3. if collection; will all elems be the same type?
-      - if all 'right' type, what about subtypes? (e.g. for Number: `NaN`, `+/-Infinity`, `Float`, Non-base 10 nums, leading 0: octal unless next num is 8 or 9)
-    4. 
+      - how do I handle the wrong number of args? (ommitted/excess)
+    2. are the argument(s) data types guaranteed?
+      - if not, how should the wrong types be handled?
+    3. if an argument is a collection...
+      - will all elements be the 'same' type?
+      - subtypes
+    4. if all arguments are the'right' type, what about subtypes? (e.g. for Number: `NaN`, `+/-Infinity`, `Float`, Non-base 10 nums, leading 0: octal unless next num is 8 or 9)
 
-  ## considerations for:
+
+  ## type-specific considerations:
 
   ### STRINGS:
   - does case matter for this problem?
-  - how should I handle "empty" strings? (`""` vs `" "`)
-  - strings w/ (leading || trailing || intermittent) whitespaces
-  - "words" with leading/trailing non-alphanumeric or non-alphabetical chrs
+  - how should I handle "empty" strings? (`""` vs. `" "`)
+  - is there a min/max length required for a string to be considered valid?
+  - how should strings w/ (leading || trailing || intermittent) whitespaces be handled?
+  - will "words" with (leading || trailing || intermittent) non-alphanumeric chrs break my solution?
   - what constitutes a WORD for this problem?
-  - is there a min/max length required for a string element to be considered?
 
   ### NUMBERS:
-  - subtypes: (ints, floats, specials, bigInts, non-base 10, etc.)
-  - +/- signed numbers (especially when dealing with indices, `slice(negNum)` vs `arr[negNum]`
-  - special numbers (`NaN -Infinity`)
+  - do we need to consider number subtypes? (e.g. ints, floats, specials, bigInts, non-base 10 nums, ...)
+  - +/- numbers (especially when dealing with indices, `slice(negNum)` vs `arr[negNum]`)
+  - do we need to consider zeros in this problem?
+  - if converting Numbers and Strings, are we concerned with mis-converting octal numbers?
 
   ### ARRAYS:
-  - "empty" arrays (empty, sparse, has non-indexed object properties)
-  - non-empty arrays with non-indexed properties (e.g.: `['a', 'b', '1.5': 'c'])
   - are all elements of the 'correct' type?
+  - "empty" arrays (empty, sparse, or having non-integer-indexed properties)
+  - non-empty arrays with non-integer-indexed properties (e.g.: `['a', 'b', '1.5': 'c']`)
+
+  - does order matter?  would sorting the input array help solve the problem?
+  - will each element in the array be unique or can we expect duplicates?
+  - is there a required min/max length for the input array to be valid?
 
   ### OBJECTS:
 
