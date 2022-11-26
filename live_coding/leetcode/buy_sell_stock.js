ab193acd1da89c056bd31f1e09dcad22d0e9df37
Joel Barton <joelbarton406@gmail.com>
2022-11-26 13:01:50 -0700

    [+] lc easy

--- live_coding/leetcode/buy_sell_stock.js
+++ live_coding/leetcode/buy_sell_stock.js
@@ -1,5 +1,3 @@
-
-
 /*
 ?:
 
@@ -49,35 +47,45 @@ tempShifted = 1
 [5,3,6,4]
 */
 
-const maxProfit = (prices) => {
-  if (prices.length < 2) return 0; // doesn't account for sparse arrays
+const maxProfit = function(prices) {
+  let maxProfit = 0;
+  let min = prices[0];
 
-  let profit = 0;
-  let left = prices[0];
-  for (let i = 1; i < prices.length - 1; i++) {
-    if ((prices[i]) < left) {
-      left = prices[i]
-    }
+  for(let i = 1; i < prices.length; ++i) {
+    let currPrice = prices[i]
+      // if currPrice is < min, we now have a new
+      if(min > currPrice) {
+          min = currPrice;
 
-    let right = prices[i + 1];
-
-    if (left > right) {
-      continue;
-    }
-
-    let temp = right - left;
-
-    if (profit < temp) {
-      profit = temp;
+      } else {
+        let newProfit = currPrice - min;
+        // if the new profit exceeds the current value of maxProfit, then we want to reassign maxProfit to new profit
+        if(newProfit > maxProfit) {
+          maxProfit = newProfit;
+      }
     }
   }
-  return profit;
+
+  return maxProfit;
 };
 
 
-/*
-if
-*/
 
+//console.log(maxProfit([7,1,5,3,6,4]))
 
-console.log(maxProfit([7,1,5,3,6,4]))
+// ADD CODE HERE
+function intersection(arrs) {
+  let [first, second, third] = arrs;
+
+  return first.reduce((acc, curr) => {
+    if (second.includes(curr) && third.includes(curr)) acc.push(curr)
+    return acc
+  }, []);
+}
+
+// Uncomment these to check your work!
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+
+console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
