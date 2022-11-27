b098d7a1eefb1114163c97be708e1a946f197cfc
Joel Barton <joelbarton406@gmail.com>
2022-11-27 14:21:07 -0700

    [+] rearrange files

--- edabit79/object_problems/first_recurrence_indices.js
+++ edabit79/object_problems/first_recurrence_indices.js
@@ -1,5 +1,8 @@
 /*
-Create a function that identifies the very first item that has recurred in the string argument passed. It returns the identified item with the index where it first appeared and the very next index where it resurfaced -- entirely as an object; or as an empty object if the passed argument is either null, undefined, empty string, or no recurring item exists.
+Create a function that identifies the very first item that has recurred in the string argument passed.
+It returns the identified item with the index where it first appeared and the very next index where it
+resurfaced -- entirely as an object; or as an empty object if the passed argument is either null,
+undefined, empty string, or no recurring item exists.
 /*
 
 /*
