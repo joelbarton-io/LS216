c4ca5b123577cca70cc26c6977cd0482bb28d6bb
Joel Barton <joelbarton406@gmail.com>
2022-12-01 10:58:14 -0700

    [+] LC easy

--- live_coding/leetcode/failed/add_binary.js
+++ live_coding/leetcode/failed/add_binary.js
@@ -4,11 +4,11 @@
 //      ^
 // res   : ''
 
-var addBinary = function(a, b) {
+var addBinary = function (a, b) {
   let aPoint = a.length - 1;
   let bPoint = b.length - 1;
   let result = '';
-  let toAdd  = 0;
+  let toAdd = 0;
   let aVal;
   let bVal;
 
@@ -17,18 +17,25 @@ var addBinary = function(a, b) {
 
     if (aPoint < 0) {
       aVal = 0;
-    } else if (bPoint < 0) {
+    }
+    if (bPoint < 0) {
       bVal = 0;
-    } else {
-      aVal = Number(a[aPoint]);
+    }
+    if (bPoint >= 0) {
       bVal = Number(b[bPoint]);
+    }
+    if (aPoint >= 0) {
+      aVal = Number(a[aPoint]);
     }
 
     sum = aVal + bVal + toAdd;
-
+    // console.log('sum: ' + sum, 'aVal: ' + aVal, 'bVal ' + bVal, 'toAdd: ' + toAdd);
     if (sum === 2) {
       toAdd = 1;
       result = '0' + result;
+    } else if (sum === 3) {
+      toAdd = 1;
+      result = '1' + result;
     } else {
       toAdd = 0;
       result = String(sum) + result;
@@ -40,5 +47,4 @@ var addBinary = function(a, b) {
 
   if (toAdd === 1) result = '1' + result;
   return result;
-
 };
